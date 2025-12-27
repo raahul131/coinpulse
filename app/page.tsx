bdce1617edb5fab1befc27c0cc850d7ef8804984
raahul131 <rryadav2025@gmail.com>
2025-12-27 16:33:58 +0545

    project init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <p  className={"text-3xl text-violet-600"}>
+        Coin Pulse
+      </p>
+    </div>
+  )
+}
+export default Page
